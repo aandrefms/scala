3efbcfc85f5f6fb3a4041c276306b7ea353e76fc
andre <andre.felippems@gmail.com>
2021-01-23 14:18:45 -0300

    23/01

--- src/com/andrefms/book11Anonymous.scala
+++ src/com/andrefms/book11Anonymous.scala
@@ -15,5 +15,30 @@ object book11Anonymous extends App{
 
   val allPairs = List(1, 2, 3).flatMap(number => List('a', 'b', 'c').map(letter => s"$number - $letter"))
   val aMapped = List(1, 2, 3).map(x => x + 1) // or: List(1, 2, 3).map(_ + 1)
-  println(aMapped)
+
+
+
+  // No NULL Values
+  // You don’t use null values in FP. Scala’s solution is to use constructs like the Option/Some/None classes.
+  // First attempt to deal with parseInt problem:
+  def toInt(s: String): Int = {
+    try {
+      Integer.parseInt(s.trim)
+    } catch {
+      case e: Exception => 0
+    }
+  }
+  // Better Solution:
+  def toInt2(s: String): Option[Int] = {
+    try {
+      Some(Integer.parseInt(s.trim))
+    } catch {
+      case e: Exception => None
+    }
+  }
+  // I've to use match expression or for-loops to handle Option output Type
+  toInt2("25") match {
+    case Some(i) => println(i)
+    case None => println("That didn't work.")
+  }
 }
